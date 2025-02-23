d9e53fa07de5b9a16fd3e026c6c4af963ef64fcb
Arc <copklient13@gmail.com>
2025-02-23 23:28:52 +0400

    Radzen template added

--- BlazorApp/BlazorApp/Program.cs
+++ BlazorApp/BlazorApp/Program.cs
@@ -6,12 +6,18 @@ using Microsoft.EntityFrameworkCore;
 using Blazored.Modal;
 using MudBlazor;
 using MudBlazor.Services;
+using Radzen;
+using Radzen.Blazor;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
 builder.Services.AddHttpClient();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -32,26 +38,39 @@ builder.Services.AddLogging(logging =>
 {
     logging.AddConsole();
 });
+builder.Services.AddRadzenComponents();
+
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(@"/app/keys"))
+    .SetApplicationName("blazorapp");
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
+
+// Миграция базы данных
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate(); // Применяет миграции при запуске
+
+    try
+    {
+        dbContext.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"[WARNING] Ошибка миграции: {ex.Message}. Возможно, БД уже обновлена.");
+    }
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
+    .AddInteractiveServerRenderMode().AddInteractiveWebAssemblyRenderMode();
 app.Run();
